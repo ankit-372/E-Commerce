17e4dcdc681de39c381634036d02e0c0ecf1ad05
pranshu123321 <pranshutiwari19052005@gmail.com>
2025-02-05 17:58:55 +0530

    generation of access and refresh token

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,20 +1,66 @@
 import User from "../models/user.model.js";
+import { redis } from "../lib/redis.js";
+import jwt from "jsonwebtoken";
+
+const generateTokens = (userId) => {
+	const accessToken = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, {
+		expiresIn: "15m",
+	});
+
+	const refreshToken = jwt.sign({ userId }, process.env.REFRESH_TOKEN_SECRET, {
+		expiresIn: "7d",
+	});
+
+	return { accessToken, refreshToken };
+};
+
+const storeRefreshToken = async (userId, refreshToken) => {
+	await redis.set(`refresh_token:${userId}`, refreshToken, "EX", 7 * 24 * 60 * 60); // 7days
+};
+
+const setCookies = (res, accessToken, refreshToken) => {
+	res.cookie("accessToken", accessToken, {
+		httpOnly: true, 
+		secure: process.env.NODE_ENV === "production",
+		sameSite: "strict", 
+		maxAge: 15 * 60 * 1000, 
+	});
+	res.cookie("refreshToken", refreshToken, {
+		httpOnly: true, 
+		secure: process.env.NODE_ENV === "production",
+		sameSite: "strict", 
+		maxAge: 7 * 24 * 60 * 60 * 1000, 
+	});
+};
 
 export const signup = async (req, res) => {
-  const { email, password, name } = req.body;
-  try {
-    const userExists = await User.findOne({ email });
+	const { email, password, name } = req.body;
+	try {
+		const userExists = await User.findOne({ email });
 
-    if (userExists) {
-      return res.status(400).json({ message: "User already exists" });
-    }
-    const user = await User.create({ name, email, password });
+		if (userExists) {
+			return res.status(400).json({ message: "User already exists" });
+		}
+		const user = await User.create({ name, email, password });
 
-    res.status(201).json({ user, message: "User created successfully" });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+		const { accessToken, refreshToken } = generateTokens(user._id);
+		await storeRefreshToken(user._id, refreshToken);
+
+		setCookies(res, accessToken, refreshToken);
+
+		res.status(201).json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			role: user.role,
+		});
+	} catch (error) {
+		console.log("Error in signup controller", error.message);
+		res.status(500).json({ message: error.message });
+	}
 };
+
+
 export const login = async (req, res) => {
   res.send("Login route called");
 };
---
+++ backend/lib/redis.js
@@ -0,0 +1,8 @@
+import Redis from "ioredis"
+import dotenv from "dotenv"
+
+dotenv.config();
+console.log(process.env.UPSTASH_REDIS_URL);
+
+export const redis = new Redis(process.env.UPSTASH_REDIS_URL);
+await redis.set("foo", "bar");
