b4e355d61c32c01381d1c39f1d04f3eaeb1c99ca
Ankit Patel <ankitkumarpatel372@gmail.com>
2025-02-08 17:00:23 +0530

    coded payment and coupon files

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,12 @@ import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from "./routes/coupon.route.js";
+import paymentRoutes from "./routes/payment.route.js";
+
 import { connectDB } from "./lib/db.js";
+
 
 dotenv.config();
 
@@ -16,6 +21,10 @@ app.use(cookieParser()); //allows you to parse the body of the request
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponRoutes);
+app.use("/api/payments", paymentRoutes);
+
 app.listen(PORT, () => {
   console.log("Server is runnig on http://localhost:", PORT);
 
